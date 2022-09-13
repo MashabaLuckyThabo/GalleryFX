886e9d7cc03d87b78aa457920be91a8f0b008e45
MashabaLuckyThabo <49552389+MashabaLuckyThabo@users.noreply.github.com>
2022-09-13 02:41:33 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,347 @@
+import React, { Fragment, Component } from 'react';
+import ImagePicker from 'react-native-image-picker';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  Button,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const options = {
+  title: 'Select Avatar',
+  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filepath: {
+        data: '',
+        uri: ''
+      },
+      fileData: '',
+      fileUri: ''
+    }
+  }
+
+  chooseImage = () => {
+    let options = {
+      title: 'Select Image',
+      customButtons: [
+        { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
+      ],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = { uri: response.uri };
+
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+        // alert(JSON.stringify(response));s
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri
+        });
+      }
+    });
+  }
+
+  launchCamera = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchCamera(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = { uri: response.uri };
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri
+        });
+      }
+    });
+
+  }
+
+  launchImageLibrary = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchImageLibrary(options, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = { uri: response.uri };
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri
+        });
+      }
+    });
+
+  }
+
+  renderFileData() {
+    if (this.state.fileData) {
+      return <Image source={{ uri: 'data:image/jpeg;base64,' + this.state.fileData }}
+        style={styles.images}
+      />
+    } else {
+      return <Image source={require('./assets/pp.png')}
+        style={styles.images}
+      />
+    }
+  }
+
+  renderFileUri() {
+    if (this.state.fileUri) {
+      return <Image
+        source={{ uri: this.state.fileUri }}
+        style={styles.images}
+      />
+    } else {
+      return <Image
+        source={require('./assets/o.jpg')}
+        style={styles.images}
+      />
+    }
+  }
+  render() {
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <View style={styles.body}>
+            <Text style={{textAlign:'center',fontSize:20,paddingBottom:10}} >Pick Images from Camera & Gallery</Text>
+            <View style={styles.ImageSections}>
+              <View>
+                {this.renderFileData()}
+                <Text  style={{textAlign:'center'}}>Base 64 String</Text>
+              </View>
+              <View>
+                {this.renderFileUri()}
+                <Text style={{textAlign:'center'}}>File Uri</Text>
+              </View>
+            </View>
+
+            <View style={styles.btnParentSection}>
+              <TouchableOpacity onPress={this.chooseImage} style={styles.btnSection}  >
+                <Text style={styles.btnText}>Choose File</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={this.launchCamera} style={styles.btnSection}  >
+                <Text style={styles.btnText}>Directly Launch Camera</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={this.launchImageLibrary} style={styles.btnSection}  >
+                <Text style={styles.btnText}>Directly Launch Image Library</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+
+  body: {
+    backgroundColor: Colors.white,
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    height: Dimensions.get('screen').height - 20,
+    width: Dimensions.get('screen').width
+  },
+  ImageSections: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    justifyContent: 'center'
+  },
+  images: {
+    width: 150,
+    height: 150,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginHorizontal: 3
+  },
+  btnParentSection: {
+    alignItems: 'center',
+    marginTop:10
+  },
+  btnSection: {
+    width: 225,
+    height: 50,
+    backgroundColor: '#DCDCDC',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+    marginBottom:10
+  },
+  btnText: {
+    textAlign: 'center',
+    color: 'gray',
+    fontSize: 14,
+    fontWeight:'bold'
+  }
+});
+
+
+
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { Button, StyleSheet, Text, View,Image } from 'react-native';
+// import { Camera } from 'expo-camera';
+// import { shareAsync } from 'expo-sharing';
+// import * as MediaLibrary from 'expo-media-library';
+// import { useEffect, useRef, useState } from 'react';
+// import { SafeAreaView } from 'react-native-web';
+
+
+// export default function App() {
+
+//   let cameraRef = useRef();
+//   const [CameraPermission,setCameraPermission]=useState();
+//   const [MediaPermission,setMediaPermission]=useState();
+//   const [photo,setPhoto]= useState();
+// useEffect(()=>{
+// (async ()=>{
+//   const cameraP =  await Camera.requestCameraPermissionsAsync();
+//   const mediaPer =  await MediaLibrary.requestPermissionsAsync();
+
+//   setCameraPermission(cameraP.status === "granted")
+//   setMediaPermission(mediaPer.status === "granted")
+// })();
+// },[])
+
+// if(CameraPermission === undefined)
+// {
+//   return <Text>Requesting permission...</Text>
+// }else if(!CameraPermission){
+//   return <Text> permission to camera no granted...</Text>
+// }
+
+// let takepic= async () =>{
+//   let options = {
+//     storageOptions: {
+//       skipBackup: true,
+//       path: 'images',
+//     },
+//   };
+
+//   let newPhoto = await cameraRef.current.takePictureAsync(options);
+//   setPhoto(newPhoto);
+// }
+
+// if(photo){
+//   let sharePic = ()=>{
+//       shareAsync(photo.uri).then(()=>{
+//         setPhoto(undefined)
+//       })
+//   };
+//   let savePhoto = ()=>{
+//     MediaLibrary.saveToLibraryAsync(photo.uri).then(()=>{
+//       setPhoto(undefined)})
+//   };
+
+//   return(
+//     <SafeAreaView style={styles.container} >
+//       <Image style={styles.preview} source={{uri: "data:image/jpg:base64," + photo.base64}}/>
+//       <Button title='share' onPress={sharePic}/>
+//       {MediaPermission ? <Button title='save' onPress={savePhoto}/>: undefined}
+//       <Button title='discard' onPress={() => setPhoto(undefined)}/>
+//     </SafeAreaView>
+//   )
+// }
+//   return (
+//     <Camera style={styles.container} ref={cameraRef}>
+//      <View  style={styles.Buttoncontainer}>
+//       <Button title='take pic' onPress={takepic}/>
+//      </View>
+//       <StatusBar style="auto" />
+//     </Camera>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+ 
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   Buttoncontainer:{
+//     backgroundColor:"#fff",
+//     alignSelf:"flex-end"
+//   },
+//   preview:{
+//     alignSelf:'stretch',
+//     flex:1
+//   }
+// });
